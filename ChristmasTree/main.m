9945d60efac140c6d5c8fafd28598b8a4b580036
Stephen Visser <visser.stephen@gmail.com>
2015-09-07 20:10:01 -0600

    Initial Commit

---
+++ ChristmasTree/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ChristmasTree
+//
+//  Created by Stephen Visser on 2015-09-07.
+//  Copyright (c) 2015 Stephen Visser. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
